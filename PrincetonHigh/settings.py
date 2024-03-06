5561baf84e91f3beb7199ba41fc6ea3756b50698
Ikechukwu Anyikwa <austineanyikwa@gmail.com>
2024-03-06 06:57:09 +0100

    new commit

--- PrincetonHigh/settings.py
+++ PrincetonHigh/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f-g-iecksp(qq#1u#v30du2yzt@e2#(f3$891r-=tsi%@c7g+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'school', 'static')]
 
 # Default primary key field type
